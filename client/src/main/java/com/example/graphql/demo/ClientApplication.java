a78a66596f2fc17c397fbf14305aefac4b89978e
Nikita Konev <nikit.cpp@yandex.ru>
2023-07-09 06:03:37 +0400

    Cleanup

--- client/src/main/java/com/example/graphql/demo/ClientApplication.java
+++ client/src/main/java/com/example/graphql/demo/ClientApplication.java
@@ -1,12 +1,8 @@
 package com.example.graphql.demo;
 
-import graphql.schema.GraphQLScalarType;
-import name.nkonev.multipart.spring.graphql.coercing.webflux.UploadCoercing;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @SpringBootApplication
 public class ClientApplication  {
@@ -16,15 +12,5 @@ public class ClientApplication  {
                 .web(WebApplicationType.NONE)
                 .run(args);
     }
-
-//    @Bean
-//    public RuntimeWiringConfigurer runtimeWiringConfigurerUpload() {
-//        GraphQLScalarType uploadScalar = GraphQLScalarType.newScalar()
-//                .name("Upload")
-//                .coercing(new UploadCoercing())
-//                .build();
-//
-//        return wiringBuilder -> wiringBuilder.scalar(uploadScalar);
-//    }
 
 }
